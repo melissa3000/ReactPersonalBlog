d802a4376791fffc3805bf76b0eb9809fed190ac
Melissa Boyer <melissaboyer3000@gmail.com>
2018-03-14 08:30:36 -0400

    Handle form submission and validate input

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -26,8 +26,8 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
         />
         <Field
@@ -40,6 +40,40 @@ class PostsNew extends Component {
   }
 }
 
+
+// Called automatically when user tries to submit the form.
+// Values is an object that contains all the different values that a user has
+// entered into a form.
+function validate(values) {
+  const errors = {}
+
+  // Validate inputs from 'values' object
+  if (!values.title || values.title.length < 3) {
+    errors.title = "Enter a title with length of at least three characters";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter some categories";
+  }
+
+  if (!values.content) {
+    errors.content = "Enter some content";
+  }
+
+  // If errors is empty, the form is fine to submit
+  // If errors has any properties, redux form assumes form is invalid and will not
+  // submit the form
+  return errors;
+
+}
+
+// Redux form has built in validation that you can wire in with validate.
+
+// validate: validate becomes just validate because of ES6
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 }) (PostsNew);
+
+
+
