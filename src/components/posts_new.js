c98f6c73707bcab48aa164a1594958b258981b31
Melissa Boyer <melissaboyer3000@gmail.com>
2018-03-13 20:53:58 -0400

    Wired in Title input field

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  // wire in the input for the Field in the form below
+  // field param contains some event handlers already
+  renderTitleField(field) {
+    return (
+      <div>
+        <input
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
       <form>
         <Field
           name="title"
-          component={}
+          component={this.renderTitleField}
         />
       </form>
     );
