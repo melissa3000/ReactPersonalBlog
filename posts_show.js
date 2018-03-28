21a77c1e4ddfb98556acec26e5ede47aea8fe14a
Melissa <melissaboyer3000@gmail.com>
2018-03-28 14:37:22 -0400

    Added Delete Post button

--- posts_show.js
+++ posts_show.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
   // When this component appears on screen, we'll automatically fetch the post and add it to the post
   // piece of state.
   componentDidMount() {
     // Used to get access to post ID in URL so that we're displaying the specified post
-    const { id} = this.props.match.params;
+    const { id } = this.props.match.params;
     this.props.fetchPost(id);
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+
+    this.props.deletePost(id, () => {
+        this.props.history.push('/');
+    });
   }
 
   render() {
@@ -22,6 +30,12 @@ class PostsShow extends Component {
     return (
       <div>
         <Link to="/">Back To Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}
+        >
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -37,4 +51,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost }) (PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost }) (PostsShow);
